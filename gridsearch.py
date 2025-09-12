0fd3608f11dd5a7ca0d0a5c587e44d987dbd750b
clairevwilson <cvwilson@andrew.cmu.edu>
2025-09-12 19:34:25 -0400

    added glac_no to gridsearch

--- gridsearch.py
+++ gridsearch.py
@@ -91,19 +91,23 @@ all_runs = []
 missing_fn = eb_prms.output_filepath + out_fp + 'missing.txt'
 
 # Dates depend on the site
-if args.site == 'Z' and args.glac_no == '01.00570':
+if args.site == 'Z':
+    args.glac_no = '01.00570'
     args.startdate = '2021-08-01'
     args.enddate = '2025-05-01'
-if args.site == 'T' and args.glac_no == '01.00570':
+if args.site == 'T':
+    args.glac_no = '01.00570'
     args.startdate = '2012-08-01'
     args.enddate = '2025-05-01'
 if args.site == 'EC':
+    args.glac_no = '01.09162'
     args.startdate = '2015-08-01'
     args.enddate = '2025-05-01'
 if args.site == 'KPS':
+    args.glac_no = '01.22193'
     args.startdate = '2015-08-01'
     args.enddate = '2025-05-01'
-    eb_prms.bias_vars = ['wind','temp','rh','SWin']
+    eb_prms.bias_vars = ['wind','temp','rh']
 
 # Loop through parameters
 for kp in params['kp']:
