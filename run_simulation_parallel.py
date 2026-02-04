bd8cd1d67e92312c92c0946b89e5d803db81cd00
clairevwilson <cvwilson@andrew.cmu.edu>
2026-02-03 19:33:05 -0500

    runs for B plotted

--- run_simulation_parallel.py
+++ run_simulation_parallel.py
@@ -16,18 +16,17 @@ import run_simulation as sim
 import pebsi.massbalance as mb
 import pebsi.input as prms
 
-n_runs_ahead = 0    # Step if you're going to run this script more than once
+n_runs_ahead = 1    # Step if you're going to run this script more than once
 
 # Read command line args
 args = sim.get_args()
 
 # Edit these
-args.startdate = '2010-04-20 00:00'
-# args.startdate = '2020-04-20 00:00'
-args.enddate = '2022-08-20 00:00'
+args.startdate = '2000-04-20 00:00'
+args.enddate = '2025-08-20 00:00'
 args.glac_no = '01.00570'
 args.use_AWS = False
-sites = ['AU'] # Sites to run in parallel ,'B','D','Z'
+sites = ['B'] # Sites to run in parallel ,'B','D','Z'
  
 # Probably do not edit
 args.store_data = True
@@ -52,7 +51,7 @@ def pack_vars():
         # Output name
         df_meta = pd.read_csv('data/glacier_metadata.csv',index_col=0,converters={0: str})
         glac = df_meta.loc[args.glac_no,'name']
-        args_run.out = f'{glac}{site}_{run_date}_base_short_'
+        args_run.out = f'{glac}{site}_{run_date}_base_long_'
 
         # Store model parameters
         store_attrs = {'kp':str(args_run.kp), 'c5':str(args_run.Boone_c5),
