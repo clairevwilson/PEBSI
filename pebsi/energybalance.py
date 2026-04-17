7a6e6afacdd72fc8d28a8aa95f87aabc0be04356
clairevwilson <cvwilson@andrew.cmu.edu>
2026-04-17 10:47:38 -0400

    testing different implementation of stability in bulk richardson

--- pebsi/energybalance.py
+++ pebsi/energybalance.py
@@ -70,6 +70,7 @@ class energyBalance():
         # time
         self.timestamp = timestamp
         self.dt = prms.dt
+        self.iters = 0
 
         # store previous timestep incoming shortwave
         if timestamp != pd.to_datetime(climate.cds.time.values[0]):
@@ -170,6 +171,9 @@ class energyBalance():
 
         # OUTPUTS
         Qm = NR + Qp + Qs + Ql + Qg
+
+        # keep track of iterations
+        self.iters += 1
 
         if mode in ['sum']:
             return Qm
@@ -459,23 +463,24 @@ class energyBalance():
             # calculate stability coefficients
             csT = KARMAN**2/(np.log(z/z0) * np.log(z/z0t))
             csQ = KARMAN**2/(np.log(z/z0) * np.log(z/z0q))
-            if RICHARDSON <= 0.01:
-                psi = 1
-            elif 0.01 < RICHARDSON <= 0.2:
-                psi = np.square(1-5*RICHARDSON)
+
+            # if RICHARDSON <= 0.01:
+            #     psi = 1
+            # elif 0.01 < RICHARDSON <= 0.2:
+            #     psi = np.square(1-5*RICHARDSON)
+            # else:
+            #     psi = 0
+
+            # Beljaars and Holtslag
+            if RICHARDSON <= 0:
+                psi = (1.0 - 15.0 * RICHARDSON)**0.5 # unstable
             else:
-                psi = 0
+                psi = np.exp(-5.0 * RICHARDSON) # stable
             
             # calculate fluxes
             Qs = density_air*CP_AIR*csT*psi*wind_2m*(self.tempC - surftemp)*np.cos(SLOPE)
             Ql = density_air*Lv*csQ*psi*wind_2m*(qz-q0)*np.cos(SLOPE)
-            # if Qs > 100: # self.timestamp > pd.to_datetime('2015-08-15') and self.timestamp < pd.to_datetime('2015-08-20'):
-            # if True:
-            #     print('Qs!', Qs, self.timestamp, self.args.site, 'density air', density_air, 'surface pressure', self.sp)
-            #     print('psi', psi, 'csT', csT, 'roughness', z0,'wind 2m', wind_2m)
-            #     print('Temp difference', self.tempC - surftemp,' surftemp', surftemp)
-            #     print()
-            #     if Qs > 100 and self.timestamp.hour < 3: assert 1==0
+
         else:
             assert 1==0, 'Choose turbulent method from MO-similarity or BulkRichardson'
         
