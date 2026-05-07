a5d0ebe2fa094f6fd8d106b7b2da7edbb933ff66
clairevwilson <cvwilson@andrew.cmu.edu>
2026-05-07 09:12:29 -0400

    added config information to output ds attributes

--- pebsi/massbalance.py
+++ pebsi/massbalance.py
@@ -2005,20 +2005,45 @@ class Output():
         with xr.open_dataset(self.out_fn) as dataset:
             ds = dataset.load()
             ds = ds.assign_attrs(glacier=args.glac_name,
+                                 id=args.rgi_id,
                                  elevation=elev,
-                                 site=str(args.site),
+                                 site=args.site,
                                  from_AWS=AWS_str,
                                  which_AWS=which_AWS,
                                  from_reanalysis=re_str,
                                  which_reanalysis=which_re,
                                  bias_corrected=corr_str,
-                                 run_start=str(args.start_date),
-                                 run_end=str(args.end_date),
+                                 sim_start=str(args.start_date),
+                                 sim_end=str(args.end_date),
                                  model_run_date=str(pd.Timestamp.today()),
                                  time_elapsed=time_elapsed,
                                  run_by=args.machine)
             if args.task_id > -1:
                 ds = ds.assign_attrs(task_id=str(args.task_id))
+
+        # list inputs that would be duplicates or unnecessary
+        skip_in_config = ['store_data','progress_bar','debug',
+                          'dates_from_data','reanalysis',
+                          'bias_vars', 'aws_elev', 'output_fn',
+                          'glac_name', 'site', 'rgi_id',
+                          'start_date','end_date','machine']
+
+        # add args that were specified in config file
+        if args.use_config:
+            import yaml
+            with open(args.config_fn) as f:
+                config_inputs = yaml.safe_load(f)
+    
+                new_attrs = {}
+                for key, value in config_inputs.items():
+                    print(key, value, key not in skip_in_config)
+                    if key not in skip_in_config:
+                        if type(value) == list:
+                            store = ', '.join(value)
+                        else:
+                            store = value
+                        new_attrs[key] = store
+            ds = ds.assign_attrs(**new_attrs)
 
         # save NetCDF
         ds.to_netcdf(self.out_fn)
