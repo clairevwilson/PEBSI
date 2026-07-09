added test glacier; removed unnecessary climate forcings; fixed precip units label

pebsi/massbalance.py
@@ -230,13 +230,13 @@ class MassBalanceDriver:
         temp_scale = jnp.linspace(SNOW_THRESHOLD_LOW, SNOW_THRESHOLD_HIGH, 20)
 
         # calculate fraction of rain
-        fraction_rain = jnp.interp(forcings.tempC, temp_scale, rain_scale)
+        fraction_rain = jnp.interp(forcings.temp, temp_scale, rain_scale)
         rain = forcings.tp * fraction_rain * DENSITY_WATER 
         snow = forcings.tp * (1-fraction_rain) * DENSITY_WATER
 
         # make sure amounts to the boundaries correctly
-        rain = jnp.where(forcings.tempC <= SNOW_THRESHOLD_LOW, 0, rain)
-        snow = jnp.where(forcings.tempC > SNOW_THRESHOLD_HIGH, 0, snow)
+        rain = jnp.where(forcings.temp <= SNOW_THRESHOLD_LOW, 0, rain)
+        snow = jnp.where(forcings.temp > SNOW_THRESHOLD_HIGH, 0, snow)
         
         return rain,snow # kg m-2
 
@@ -260,7 +260,7 @@ class MassBalanceDriver:
         # grab forcings objects we need
         time_idx = forcings.time_idx
         wind = forcings.wind 
-        tempC = forcings.tempC
+        temp = forcings.temp
         bcwet = forcings.bcwet
         ocwet = forcings.ocwet 
         dustwet = forcings.dustwet
@@ -273,14 +273,14 @@ class MassBalanceDriver:
             new_density = params.constant_snowfall_density
         else:
             # CROCUS formulation of density (Vionnet et al. 2012)
-            new_density = jnp.maximum(109 + 6 * (tempC - 0.) + 26 * wind ** 0.5, 50)
+            new_density = jnp.maximum(109 + 6 * (temp - 0.) + 26 * wind ** 0.5, 50)
         
         # check if using constant grain size for new snow
         if params.constant_freshgrainsize:
             new_grainsize = params.constant_freshgrainsize
         else:
             # CLM formulation of grain size (CLM5.0 Documentation)
-            airtemp = tempC
+            airtemp = temp
             new_grainsize = jnp.select(
                 [airtemp <= -30, airtemp < 0],
                 [54.5, 54.5 + 5 * (airtemp + 30)],
@@ -300,9 +300,9 @@ class MassBalanceDriver:
 
         # pack properties of new layer into a namespace
         new_layer = {
-            'ltemp': tempC,
+            'ltemp': temp,
             'lheight': total_snowfall / new_density,
-            'ltype': jnp.full_like(tempC, 0),
+            'ltype': jnp.full_like(temp, 0),
             'lice': total_snowfall,
             'ldensity': new_density,
             'lgrainsize': new_grainsize,
@@ -310,9 +310,9 @@ class MassBalanceDriver:
             'lOC': new_OC,
             'ldust': new_dust,
             'lage': new_age,
-            'lwater': jnp.full_like(tempC, 0),
-            'lrefreeze': jnp.full_like(tempC, 0),
-            'ldrefreeze': jnp.full_like(tempC, 0)
+            'lwater': jnp.full_like(temp, 0),
+            'lrefreeze': jnp.full_like(temp, 0),
+            'ldrefreeze': jnp.full_like(temp, 0)
         }
         
         # define conditions for making a new layer for accumulation
@@ -1240,7 +1240,7 @@ class MassBalanceDriver:
         dt = params.dt
 
         # get temperatures
-        airtemp = forcings.tempC
+        airtemp = forcings.temp
         surftemp = state.surftemp
 
         # find fresh snow grainsize

util/config.py
@@ -153,17 +153,22 @@ class Config():
             all_regions = np.unique([f[:2] for f in args.rgi_ids])
             assert len(all_regions) < 2, 'PEBSI is only set up to run a single region'  
             args.rgi_region = int(all_regions[0])
+
         else:
             # no rgi_ids specified: open the RGI data for this region
             region = str(args.rgi_region).zfill(2)
             all_regions = os.listdir(args.rgi_fp)
             csv_path = [f for f in all_regions if f.startswith(region) and f.endswith('csv')][0]
             df = pd.read_csv(args.rgi_fp + csv_path)
-            df = df.loc[(df['Area'] > 14) & (df['Area'] < 15)]
 
             # find all glaciers in this region
             all_ids = [f.split('-')[-1] for f in df['RGIId']]
             args.rgi_ids = all_ids
+
+        # test glacier (region 00) always uses the sample AWS file
+        if args.rgi_region == 0:
+            args.use_aws = True
+            args.aws_elev = 1232
 
         # set method_distribute to points if specified sites
         if args.sites is not None: