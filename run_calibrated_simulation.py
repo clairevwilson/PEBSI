b300d4c9287ba7c97c86e33fdf60e53e9e54d96a
clairevwilson <cvwilson@andrew.cmu.edu>
2026-08-19 11:48:26 -0400

    added heatwave/wildfire sims to calibrated sim

--- run_calibrated_simulation.py
+++ run_calibrated_simulation.py
@@ -10,7 +10,6 @@ import yaml
 import os
 
 params_fn = 'project/calibrated_grid.pkl'
-out_config_fn = 'config_calibrated.yaml'
 
 configs = {}
 
@@ -46,20 +45,45 @@ configs['constant_freshgrainsize'] = 54.5
 
 # CONFIGURATION
 configs['start_date'] = '2000-04-01'
-configs['end_date'] = '2025-09-01'
+configs['end_date'] = '2003-09-01'
 configs['temporal_chunk_years'] = 5
 configs['debug'] = True
 configs['store_data'] = True
 configs['progress_bar'] = False
 configs['store_vars'] = ['MB','EB','layers','climate']
+configs['bias_vars'] = ['temp']
 
 # FILEPATHS
 configs['climate_fp'] = '/ocean/projects/ees260009p/cwilson4/climate_data/'
 configs['rgi_fp'] = '/ocean/projects/ees260009p/cwilson4/RGI/rgi60/00_rgi60_attribs/'
-configs['output_fp'] = '/ocean/projects/ees260009p/cwilson4/Output/calibrated_all/'
 configs['cop30_vrt_path'] = '/ocean/projects/ees260009p/cwilson4/data/dems/COP30/COP30_reg01.vrt'
 configs['shading_fp'] = '/ocean/projects/ees260009p/cwilson4/data/shading/'
 configs['ice_albedo_fn'] = '/ocean/projects/ees260009p/cwilson4/data/ice_albedo/{gid}_albedo.tif'
+
+configs['deposition_data'] = 'UKESM'
+
+# SPECIAL SIMS
+sim_type = os.environ.get('SIM_TYPE', 'base')
+
+if sim_type == 'base':
+    configs['ukesm_fp'] = '../UKESM/dr401_GFED/'
+
+elif sim_type == 'no_fires':
+    configs['ukesm_fp'] = '../UKESM/dw068_nofires/'
+
+elif sim_type == 'no_heatwaves':
+    configs['ukesm_fp'] = '../UKESM/dr401_GFED/'
+    configs['secondary_climate_string'] = '_no_heatwaves'
+    configs['secondary_vars'] = ['temp','SWin','LWin']
+    
+elif sim_type == 'no_either':
+    configs['ukesm_fp'] = '../UKESM/dw068_nofires/'
+    configs['secondary_climate_string'] = '_no_heatwaves'
+    configs['secondary_vars'] = ['temp','SWin','LWin']
+
+configs['output_fp'] = f'/ocean/projects/ees260009p/cwilson4/Output/extremes/{sim_type}/'
+out_config_fn = f'{sim_type}.yaml'
+
 
 with open(out_config_fn, 'w') as f:
     yaml.dump(configs, f, sort_keys=False)
